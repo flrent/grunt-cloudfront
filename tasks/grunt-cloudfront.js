e119f743b3c2b6ecda0cfce55cc23fef84f4ab78
flrent <flamoureux@payrollhero.com>
2013-05-14 15:57:43 -0700

    Added grunt task file

---
+++ tasks/grunt-cloudfront.js
@@ -0,0 +1,92 @@
+/*
+ * grunt-cloudfront
+ * @author Florent Lamoureux (@flrent)
+ * Company PayrollHero.com
+ * http://github.com/payrollhero/grunt-cloudfront
+ *
+ * Copyright (c) 2013 PayrollHero.com
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  var util = require('util'),
+      AWS = require('aws-sdk'),
+      _ = require("underscore");
+
+  process.on('SIGINT', function() {
+    grunt.log.writeln('').write('Shutting down the amazon Cloudfront task...').ok();
+    process.exit();
+  });
+
+  grunt.registerMultiTask('cloudfront', 'Cloudfront cache invalidating task', function() {
+    var done = this.async(),
+        options = this.options(),
+        version = options.version;
+
+    AWS.config.update({
+      region:options.region,
+      accessKeyId: options.credentials.accessKeyId,
+      secretAccessKey: options.credentials.secretAccessKey
+    });
+    var CloudFront = new AWS.CloudFront();
+
+    if(options.listDistributions) {
+      CloudFront.listDistributions({},function(err, data) {
+        if(err) {
+          grunt.log.errorlns(err);
+          done(false);
+        }
+        else {
+          var distributions = data.Items;
+          for(var i=0;i<distributions.length;i++) {
+            grunt.log.writeln(util.inspect(distributions[i]));
+          }
+          done();
+        }
+      });
+    }
+    else if(options.listInvalidations) {
+      CloudFront.listInvalidations({
+          DistributionId:options.distributionId
+        },
+        function(err, data) {
+          if(err) {
+            grunt.log.errorlns(util.inspect(err));
+          }
+          else {
+            grunt.log.writeln(util.inspect(data));
+          }
+      });
+    }
+
+    if(!_.isUndefined(version)) {
+      grunt.log.writeln("Version number detected. Changing "+this.data.Paths.Items.length+" filenames.");
+      _.each(this.data.Paths.Items, function(file, i, files) {
+
+        var lastDot = file.lastIndexOf('.'),
+            extension = file.slice(lastDot,file.length),
+            fileName = file.slice(0, lastDot),
+            newFileName = fileName+"-"+version+extension;
+
+          grunt.log.writeln(fileName + " -> " + newFileName);
+      });
+    }
+
+    CloudFront.createInvalidation({
+        DistributionId:options.distributionId,
+        InvalidationBatch:this.data
+      }, function(err, data) {
+        if(err) {
+          grunt.log.error("Invalidation failed : "+err.message);
+          done(false);
+        }
+        else {
+          grunt.log.write("Invalidation succeeded. Please wait a few minutes.").ok();
+          console.log(data);
+          done();
+        }
+      });
+  });
+};
